65ee950c911701a9c55d59219c344585d7813223
Grant Watson <gwatson117@gmail.com>
2021-11-14 19:29:18 -0700

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: "Gatsby + Netlify CMS Starter",
+    title: "Grant Watson",
     description:
-      "This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.",
+      "This is the portfolio website for Grant Watson. Grant Watson is a software engineer that specializes in web development. Grant Watson utilizes C#, Blazor, ReactJS as his main frameworks for appliciations. ",
   },
   plugins: [
     "gatsby-plugin-react-helmet",
